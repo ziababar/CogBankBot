b85db30896577eac579b1e436ebb51ee3a96236a
ziababar <zbabar@gmail.com>
2017-12-18 22:46:47 -0500

    wrong .sln file

---
+++ CogBankBot/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CogBankBot
+{
+    public class Status
+    {
+         public object[] Messages { get; set; }
+         public int ReturnCode { get; set; }
+    }
+}
