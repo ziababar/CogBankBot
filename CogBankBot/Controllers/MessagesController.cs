b401b93ec3975c034f9e0d9d8e63db2a575889d2
ziababar <zbabar@gmail.com>
2017-12-20 18:58:55 -0500

    initial transaction dialog

--- CogBankBot/Controllers/MessagesController.cs
+++ CogBankBot/Controllers/MessagesController.cs
@@ -18,8 +18,8 @@ namespace CogBankBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
-                // await Conversation.SendAsync(activity, () => new WineSearchDialog());
+                // await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.CogBankBotDialog());
             }
             else
             {
---
+++ CogBankBot/TransactionType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CogBankBot
+{
+    [Serializable]
+    public class TransactionType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public static List<TransactionType> CreateTransactionsList()
+        {
+            List<TransactionType> transactionList = new List<TransactionType>();
+
+            TransactionType A = new TransactionType();
+            TransactionType B = new TransactionType();
+
+            A.Id = 0;
+            A.Name = "Balance Inquiry";
+            A.Description = "Transaction for Balance Inquiry";
+
+            B.Id = 1;
+            B.Name = "Funds Transfer";
+            B.Description = "Transaction for Funds Transfer";
+
+            transactionList.Add(A);
+            transactionList.Add(B);
+
+            return transactionList;
+        }
+
+    }
+}
